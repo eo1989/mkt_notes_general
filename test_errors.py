1b7529a4c4de6ed44056a41292a964b69553dcaa
eo1989 <7108365+eo1989@users.noreply.github.com>
2024-09-10 13:39:25 -0400

    playing with IV surfaces today

--- test_errors.py
+++ test_errors.py
@@ -1,4 +1,5 @@
-import pytest  # noqa: INP001
+# ruff: noqa: I001,INP001
+import pytest
 from box import BoxFullError, Fruit
 
 
@@ -11,3 +12,5 @@ def test_box_full_error(fruit):
     with pytest.raises(BoxFullError) as exc_info:
         raise BoxFullError(fruit)
     assert str(exc_info.value) == "Can't add apple to the box as it is full"
+
+    ">= *** "
